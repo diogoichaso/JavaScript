bffd13ba2984d59a380d232456579e2d86411bb5
Diogo Fraga Rocha Ichaso <96790510+diogoichaso@users.noreply.github.com>
2022-06-10 22:04:24 -0300

    atualização

--- 3-Variaveis.js
+++ 3-Variaveis.js
@@ -3,9 +3,25 @@ Declaração de Variáveis
 
 var variável
 */
-
+console.log('Number')
 var x = 2
 var y = 1
-console.log(x + y)
+console.log(x)
+console.log(y)
 x = 1
 console.log(x + y)
+console.log()
+
+console.log('String')
+var frase = 'Teste de String'
+console.log(frase)
+console.log(frase.toLocaleLowerCase())
+
+console.log()
+console.log('Conversão de Tipos: ')
+console.log(String(2) + 2)
+console.log(Number("5") + 5)
+console.log(typeof("4"))
+console.log(typeof(3))
+console.log(typeof(true))
+console.log(!false)
